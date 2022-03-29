16a47aef26f8f83d8d8735ade869e4aa96bac89b
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-03-29 11:29:01 -0600

    Feat: prints board

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,17 @@ class Board
     @board = {"A" => [".", ".", ".", ".", ".", "."], "B" => [".", ".", ".", ".", ".", "."], "C" => [".", ".", ".", ".", ".", "."], "D" => [".", ".", ".", ".", ".", "."], "E" => [".", ".", ".", ".", ".", "."], "F" => [".", ".", ".", ".", ".", "."], "G" => [".", ".", ".", ".", ".", "."]} 
   end 
 
+  def print_board
+    @board.keys.each do |key|
+      print key
+    end
+    puts
+    7.times do |index|
+      @board.keys.each do |key|
+        print @board[key][index]
+      end
+      puts 
+    end
+  end 
+
 end
